b740b8e78fc93b548923acbde4500be48f863f04
LimJeonghyun <22000617@handong.edu>
2022-01-17 22:42:47 +0900

    Week2 (Jan 17th)

--- Week2/Baekjoon_1316.cpp
+++ Week2/Baekjoon_1316.cpp
@@ -1,5 +1,10 @@
+// pyhton : 68ms c++: 0ms
+// vector를 여러번 쓸 때는 vector.clear()는 필수!
+
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
 // 첫줄에 그룹 단어의 개수 출력
@@ -31,30 +36,41 @@ int main(){
             for(int j=0; j<words.length(); j++){
                 if(find(word.begin(), word.end(), words[j]) !=  word.end()) continue; // 같은 글자 없는거
                 word.push_back(words[j]);
+                // cout << words[j] << endl;
                 tmp = 0;
                 tmpc = 0;
+                result = true;
                 while(words.find(words[j], pos) != string::npos){
-                    cout << pos << words[j];
-                    tmpw += words[j];
-                    if(words.find(tmpw) == string::npos){
+                    pos= words.find(words[j], pos)+1;
+                    // cout << pos << words[j];
+                    if(tmp!=0&&tmp+1 != pos){
                         result = false;
                         break;
                     }
-                    cout << "a: " <<tmpw << endl;
                     tmp = pos;
-                    pos= words.find(words[j], pos)+1;
-                    if(pos >= words.size()) break;
-                    tmpc++;
+                //     tmpw += words[j];
+                //     if(words.find(tmpw) == string::npos){
+                //         result = false;
+                //         break;
+                //     }
+                //     cout << "a: " <<tmpw << endl;
+                //     tmp = pos;
+                //     if(pos >= words.size()) break;
+                //     tmpc++;
                 }
                 if(result == false) break;
+                // cout << "\n";
             }
-            if(result == true) count++;
-            else cout << false;
+            if(result == true) {
+                // cout << "true";
+                count++;
+            }
+            // else cout << "false";
         }
         else count++;
+        word.clear();
         words = "";
     }
-    cout << "\n" << count;
-
+    cout << count <<endl;
     return 0;
 }
