3e459a20ce2e91951c945dbb0c89b1fbcfc2e9e2
LimJeonghyun <22000617@handong.edu>
2022-02-22 20:18:05 +0900

    Week4 (Feb 22th)

--- Week4/Baekjoon_10757.cpp
+++ Week4/Baekjoon_10757.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main(){
-    // test
+    string a, b;
+    int sum=0;
+    string result;
+    cin >> a >> b;
+    while(!a.empty() || !b.empty() || sum){
+        if(!a.empty()){
+            sum+=a.back()-'0';
+            a.pop_back();
+        }
+        if(!b.empty()){
+            sum+=b.back()-'0';
+            b.pop_back();
+        }
+        result.push_back((sum%10)+'0');
+        sum/=10;
+    }
+    reverse(result.begin(), result.end());
+    cout << result << endl;
+    cout << a << b;
     return 0;
 }
