3d202642cd44385e88826df4fb5ec3eb51fb785b
LimJeonghyun <ljh01030913@gmail.com>
2024-08-27 09:12:12 +0900

    Aug 27th 2024

---
+++ Practice/B_2741.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+
+    int n;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << '\n';
+    }
+}
