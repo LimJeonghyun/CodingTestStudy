e2e5c478b874aceccf905fb0573d2ebee54c1333
LimJeonghyun <ljh01030913@gmail.com>
2024-09-02 15:15:29 +0900

    Sep 2nd 2024

---
+++ Practice/B_4101.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+
+    while (1)
+    {
+        int a, b;
+
+        cin >> a >> b;
+
+        if (a == 0 && b == 0)
+        {
+            break;
+        }
+
+        if (a > b)
+        {
+            cout << "Yes" << '\n';
+        }
+        else
+        {
+            cout << "No" << '\n';
+        }
+    }
+}
