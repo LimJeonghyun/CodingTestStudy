e985f4fe9ec0a50c427877964e2d0ecc9013ef39
LimJeonghyun <ljh01030913@gmail.com>
2024-10-03 16:12:47 +0900

    fix : Oct 3rd 2024

--- BinarySearch/B_14575.cpp
+++ BinarySearch/B_14575.cpp
@@ -1,54 +1,68 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cout.tie(NULL);
-    cin.tie(NULL);
-
     int n, t;
-    long long alcohol[1001][2];
-    int result = -1;
+    long long total_min = 0, total_max = 0;
+    vector<pair<long long, long long>> student;
 
-    cin >>
-        n >> t;
+    cin >> n >> t;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0, l, r; i < n; i++)
     {
-        cin >> alcohol[i][0] >> alcohol[i][1];
+        cin >> l >> r;
+
+        student.push_back({l, r});
+        total_min += l;
+        total_max += r;
+    }
+
+    if (total_min > t || total_max < t)
+    {
+        cout << -1 << '\n';
+        return 0;
     }
 
     long long left = 1;
     long long right = 1000000000000;
+    long long result = 0;
 
     while (left <= right)
     {
         long long mid = (left + right) / 2;
-        long long max_v = 0;
-        long long min_v = 0;
+        long long sum = 0;
         bool flag = false;
 
         for (int i = 0; i < n; i++)
         {
-            if (mid < alcohol[i][0])
+            if (mid < student.at(i).first)
             {
                 flag = true;
                 break;
             }
-            max_v += (min(mid, alcohol[i][1]));
-            min_v += alcohol[i][0];
+            if (mid >= student.at(i).first && mid <= student.at(i).second)
+            {
+                sum += mid;
+            }
+            else if (mid > student.at(i).second)
+            {
+                sum += student.at(i).second;
+            }
         }
         if (flag)
         {
             left = mid + 1;
             continue;
         }
-        if (min_v <= t && t <= max_v)
+
+        if (sum >= t)
         {
-            result = mid;
             right = mid - 1;
+            result = mid;
         }
         else
         {
