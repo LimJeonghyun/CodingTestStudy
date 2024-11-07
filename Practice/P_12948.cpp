26ef7fd903d28b8d7ba83df32c1196f0df3f1cff
LimJeonghyun <ljh01030913@gmail.com>
2024-11-07 13:44:24 +0900

    Nov 7th 2024

---
+++ Practice/P_12948.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number)
+{
+    string answer = "";
+    int idx = 0;
+
+    for (int i = 0; i < phone_number.size() - 4; i++)
+    {
+        answer += "*";
+        idx++;
+    }
+    for (int i = idx; i < phone_number.size(); i++)
+    {
+        answer += phone_number[i];
+    }
+    return answer;
+}
