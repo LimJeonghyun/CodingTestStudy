411b2038a599dfd67cf393940770d2f83fcd8598
LimJeonghyun <ljh01030913@gmail.com>
2024-12-01 13:54:04 +0900

    Dec 1st 2024

---
+++ Programmers/P_120814.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+
+    answer += n / 7;
+
+    if (n % 7)
+    {
+        answer += 1;
+    }
+
+    return answer;
+}
