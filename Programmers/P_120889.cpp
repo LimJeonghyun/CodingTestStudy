5c60ece1a6b4f630dc137b22759f2873f61595d4
LimJeonghyun <ljh01030913@gmail.com>
2024-12-19 12:44:12 +0900

    Dec 19th 2024

---
+++ Programmers/P_120889.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> sides)
+{
+    int answer = 0;
+
+    sort(sides.begin(), sides.end());
+
+    if (sides.at(2) < sides.at(0) + sides.at(1))
+    {
+        answer = 1;
+    }
+    else
+    {
+        answer = 2;
+    }
+
+    return answer;
+}
