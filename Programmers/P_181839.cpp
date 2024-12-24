6bc2aa25ae92c94eefa2069f2710c35408ac1660
LimJeonghyun <ljh01030913@gmail.com>
2024-12-25 00:00:10 +0900

    Dec 25th 2024

---
+++ Programmers/P_181839.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b)
+{
+    int answer = 0;
+
+    if (a % 2 == 0 && b % 2 == 0)
+    {
+        answer = abs(a - b);
+    }
+    else if ((a % 2 != 0 && b % 2 == 0) || (a % 2 == 0 && b % 2 != 0))
+    {
+        answer = 2 * (a + b);
+    }
+    else if (a % 2 != 0 && b % 2 != 0)
+    {
+        answer = a * a + b * b;
+    }
+
+    return answer;
+}
+
+// 12.26
